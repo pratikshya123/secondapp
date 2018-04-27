17e0798f8a7d15700ca1fb34e48a8164750aaa08
pratikshya123 <pratixyamahrn@gmail.com>
2018-04-27 08:32:24 +0545

    secondapp update

--- src/component/Login.js
+++ src/component/Login.js
@@ -33,18 +33,28 @@ class Login extends Component{
             avatar={Logo}
 			/>
 			<CardTitle title="Login"/>
-				<TextField
+				<CardText>
+
+				<TextField type="Email"
+				fullWidth={true}
+				floatingLabelText="Email Address"
 			      hintText="Email" value={this.state.email}
 			      onChange={(e)=> this.onEmailChange(e)}
 			    /><br />
 			    <br />
-			     <TextField
+			     <TextField type="password"
+			     fullWidth={true}
+			     floatingLabelText="Api Key"
 			      hintText="Api Key" value={this.state.apikey}
 			      onChange={(e) =>this.onApikeyChange(e)}
 			    /><br />
 
 			    <br />
-			     <button onClick={() => this.onsubmit()}>submit</button>
+			     </CardText>
+			      <CardActions style={this.floatActionButtonRight}>
+			       <RaisedButton label="Submit" primary={true} onClick={ () => this.submitForm() }/>
+			     
+			     </CardActions>
 			     </Card>
 
 			</div>
